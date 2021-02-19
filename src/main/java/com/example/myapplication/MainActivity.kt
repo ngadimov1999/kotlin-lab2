3f43b71331802d58e0f2932a4a65ed00aef89306
nabi_koban <52134694+ngadimov1999@users.noreply.github.com>
2021-02-19 23:14:24 +0500

    Add files via upload

--- src/main/java/com/example/myapplication/MainActivity.kt
+++ src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.util.Log
 
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
@@ -11,7 +12,32 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(findViewById(R.id.toolbar))
+        Log.d("MainActivity","оно рождается)))")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("MainActivity","оно дышит)))")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("MainActivity","оно работает)))")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("MainActivity","оно свернулось(((")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("MainActivity","оно остановилось...")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("MainActivity","умер")
     }
 
 }
